51eeb1f151599621d1981dbfbb23ec7f7e4aac85
Kristen <kleigh.oconnell@gmail.com>
2021-04-14 21:03:31 -0500

    edit app functions

--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
 const express = require("express");
 const app = express();
+const flips = require("./data/flips-data");
+const counts = require("./data/counts-data");
+app.use(express.json());
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+//Return one count
+app.use("/counts/:countId", (req, res, next) => {
+  const { countId } = req.params;
+  const foundCount = counts[countId];
+
+  if (foundCount === undefined) {
+    next(`Count id not found: ${countId}`);
+  } else {
+    res.json({ data: foundCount });
+  }
+})
+
+//Return all counts
+app.use("/counts", (req, res) => {
+  res.json({ data: counts });
+})
+
+//API for a specific flip 
+app.use("/flips/:flipId", (req, res, next) => {
+  const { flipId } = req.params;
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+
+  if (foundFlip) {
+    res.json({ data: foundFlip });
+  } else {
+    next(`Flip id not found: ${flipId}`);
+  }
+})
+
+//Use API
+app.get("/flips", (req, res) => {
+  res.json({ data: flips });
+});
 
 // Not found handler
 app.use((request, response, next) => {
